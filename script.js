05bca815ce21324c9acc3fb69149c49373277639
Jessica Abdoulahi <abdoulahi.jessica@gmail.com>
2023-05-02 00:30:47 +0200

    Ajout d'une section pour des photos

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let button = document.getElementById("my-button");
 button.addEventListener("click", function() {
     button.style.background = "pink";
 });
+
+const title = document.getElementById('title');
+window.addEventListener('load', () => {
+    titre.style.animationPlayState = 'running';
+  });
