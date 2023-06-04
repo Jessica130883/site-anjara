d77d73a86575326ff1413cea32b582d8f1ea5413
Jessica Abdoulahi <abdoulahi.jessica@gmail.com>
2023-06-04 10:10:34 +0200

    Modification des deux div annonçant la tracklist avec les titre des chansons - ajout d'un script dans le fichier JS

--- script.js
+++ script.js
@@ -3,7 +3,27 @@ button.addEventListener("click", function() {
     button.style.background = "pink";
 });
 
-const title = document.getElementById('title');
-window.addEventListener('load', () => {
-    titre.style.animationPlayState = 'running';
+const tracklistTitle = document.querySelector('.tracklist-title');
+const songTitles = document.querySelectorAll('.song-title');
+let currentIndex = 0;
+
+function rotateSongs() {
+  tracklistTitle.style.display = 'inline-block';
+  songTitles.forEach((title, index) => {
+    if (index === currentIndex) {
+      title.style.display = 'inline-block';
+    } else {
+      title.style.display = 'none';
+    }
   });
+
+  currentIndex++;
+  if (currentIndex === songTitles.length) {
+    currentIndex = 0;
+  }
+
+  setTimeout(rotateSongs, 2000); // Changez 2000 pour définir la durée de chaque titre de chanson (en millisecondes)
+}
+
+rotateSongs();
+
